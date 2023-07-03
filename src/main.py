6b34107fabbd305decf775689ac0dc63798f9cab
Podlesnyy Andrey <podlesniy@gmail.com>
2023-07-03 11:15:07 +0300

    convert to wav

--- src/main.py
+++ src/main.py
@@ -1,21 +1,66 @@
 import os
 import argparse
+import fnmatch
+import subprocess
+
 
 
 def get_child_directories(directory_path):
-    try:
-        return [name for name in os.listdir(directory_path) if os.path.isdir(os.path.join(directory_path, name))]
-    except FileNotFoundError:
-        return "The provided directory does not exist."
-    except NotADirectoryError:
-        return "The provided path is not a directory."
-    except PermissionError:
-        return "Permission denied for the provided directory."
+    return [os.path.join(directory_path, name) for name in os.listdir(directory_path) if
+            os.path.isdir(os.path.join(directory_path, name))]
+
+
+def create_directory(input_dir, output_dir):
+    input_dir_name = os.path.basename(os.path.normpath(input_dir))
+    new_dir_path = os.path.join(output_dir, input_dir_name)
+    os.makedirs(new_dir_path, exist_ok=True)
+    return new_dir_path
+
+
+def find_m4a_files(directory_path):
+    m4a_files = []
+
+    for root, dirnames, filenames in os.walk(directory_path):
+        for filename in fnmatch.filter(filenames, '*.m4a'):
+            relative_path = os.path.relpath(os.path.join(root, filename), directory_path)
+            m4a_files.append(relative_path)
+
+    return m4a_files
+
+
+def create_wav_file(dir_with_zoom, output_dir, m4afile):
+    m4a_dir, m4a_filename = os.path.split(m4afile)
+
+    zoom_m4a_file = os.path.join(dir_with_zoom, m4afile)
+    wav_filename = os.path.splitext(m4a_filename)[0] + '.wav'
+    output_dir_path = os.path.join(output_dir, m4a_dir)
+    if not os.path.exists(output_dir_path):
+        os.makedirs(output_dir_path)
+
+    output_file_path = os.path.join(output_dir_path, wav_filename)
+
+    command = ["ffmpeg", "-i", zoom_m4a_file, "-ar", "16000", "-ac", "1", "-c:a", "pcm_s16le", output_file_path]
+    if not os.path.isfile(output_file_path):
+        print(f'Converting {output_file_path}')
+        subprocess.run(command, check=True, stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT)
+        print(f'Converted {output_file_path}')
+    else:
+        print(f'Already converted {output_file_path}')
+    return output_file_path
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="List child directories of a given directory")
-    parser.add_argument("-p", "--path", type=str, help="The path to the directory")
+    parser.add_argument("-pz", "--pathZoom", type=str, help="The path to the Zoom directory")
+    parser.add_argument("-po", "--pathOutput", type=str, help="The path to the Output directory")
     args = parser.parse_args()
 
-    print(get_child_directories(args.path))
+    zooms = get_child_directories(args.pathZoom)
+    for dir_with_zoom in zooms:
+        print(f'Processing {dir_with_zoom}')
+        output_dir = create_directory(dir_with_zoom, args.pathOutput)
+        print(f'Created {output_dir}')
+        m4a_files = find_m4a_files(dir_with_zoom)
+        for m4a_file in m4a_files:
+            print(f'Processing {m4a_file}')
+            create_wav_file(dir_with_zoom, output_dir, m4a_file)
